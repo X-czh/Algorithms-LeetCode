726bb77f31770c9f976062a4be925552c1f57093
X-czh <m1a2@outlook.com>
2020-01-20 10:14:58 -0600

    add 006

---
+++ 000-099/006-ZigZag Conversion.cpp
@@ -0,0 +1,22 @@
+// Time complexity: O(n).
+// Space complexity: O(n).
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1) return s;
+        
+        vector<string> rows(min(numRows, int(s.size())));
+        int curRow = 0;
+        bool goingDown = false;
+        for (char c : s) {
+            rows[curRow] += c;
+            if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+            curRow += goingDown ? 1 : -1;
+        }
+        
+        string ans;
+        for (string& row : rows) ans += row;
+        return ans;
+    }
+};
